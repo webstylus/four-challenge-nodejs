0aec79c403024480529dc2f10e6765525256ee67
Rafael Pacífico <rafael@webstylus.com.br>
2021-11-29 21:55:12 -0300

    Add fix

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -8,10 +8,8 @@ class CreateUserController {
   handle(request: Request, response: Response): Response {
     try {
       const { name, email } = request.body;
-
-      return response
-        .status(201)
-        .json(this.createUserUseCase.execute({ name, email }));
+      const user = this.createUserUseCase.execute({ email, name });
+      return response.status(201).json(user);
     } catch (error) {
       return response.status(400).json({ message: error.message });
     }
